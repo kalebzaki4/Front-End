fc6c2f3e78b2cefb6cfb48aa2cd191a7d93203dd
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-06-20 00:05:55 -0300

    Codigo em script e estilo do pixelart
    Codigo em script e estilo do pixelart no script

---
+++ Gerador-de-Pixelart/index.js
@@ -0,0 +1,71 @@
+let container = document.querySelector(".container")
+let gridButton = document.getElementById("submit-grid")
+let clearGridButton = document.getElementById("clear-grid")
+let gridWidth = document.getElementById("width-range")
+let gridHeight = document.getElementById("height-range")
+let colorButton = document.getElementById("color-input")
+let eraseBtn = document.getElementById("erase-btn")
+let paintBtn = document.getElementById("paint-btn")
+let widthValue = document.getElementById("widht-value")
+let heightValue = document.getElementById("height-value")
+
+let events = {
+    mouse: {
+        down: "mousedown", 
+        move: "mousemove",
+        up: "mouseup",
+    },
+    touch: {
+        down: "touchstart",
+        mobe: "touchmove",
+        up: "touchend",
+    },
+}
+
+let deviceType = ""
+
+let draw = false
+let erase = false
+
+const isTouchDevice = () => {
+    try{
+        document.createEvent("TouchEvent")
+        deviceType = "touch"
+        return true
+    } catch (e){
+        deviceType = "mouse"
+        return false
+    }
+};
+
+isTouchDevice()
+
+gridButton.addEventListener("click", ()=>{
+    container.innerHTML = ""
+    let count = 0 
+    for(let i = 0; i < gridHeight.value; i++){
+        count +=2 
+        let div = document.createEvent("div")
+        div.classList.add("gridRow")
+
+        for(let j=0; j < gridWidth.value; j++) {
+            count+=2
+            let col = document.createEvent("div")
+            col.classList.add("gridCol")
+            col.setAttribute("id", `gridCol${count}`);
+            col.addEventListener(events[deviceType].down, ()=>{
+                draw = true;
+                if(erase){
+                    col.style.backgroundColor = "transparent"
+                } else {
+                    col.style.backgroundColor = colorButton.value;
+                }
+            });
+
+            col.addEventListener(events[deviceType].move, (e)=>{
+                let elemntId = document.elementFromPoint()
+            })
+            
+        }
+    }
+})
